220d832dcd34d14d50e5384ab43523dd31c99c43
Preetam Jinka <pj@preet.am>
2019-07-11 22:45:05 -0700

    new landing page and tweak editor page

--- web/js/index.js
+++ web/js/index.js
@@ -26,6 +26,10 @@ var Editor = {
 	},
 	view: function(vnode) {
 		return [
+			m("div.cn-editor-statusbar", [
+				m("a.cn-statusbar-site-name", {oncreate: m.route.link, href: "/"}, "Contrast"),
+				m("div.cn-statusbar-status-text", vnode.state.status),
+			]),
 			m("textarea", {
 				oninput: function(e) {
 					vnode.state.md = e.target.value;
@@ -51,7 +55,6 @@ var Editor = {
 				}
 			}, vnode.state.md),
 			m("div.result", m.trust(vnode.state.html)),
-			m("div.status", vnode.state.status)
 		]
 	}
 }
@@ -64,14 +67,14 @@ var NotesList = {
 			if (key.startsWith("note-")) {
 				let noteName = key.replace("note-", "");
 				list.push(m("li", [
-					m("a.sn-note-list-link", {oncreate: m.route.link, href: "/edit/"+noteName}, noteName),
+					m("a.cn-note-list-link", {oncreate: m.route.link, href: "/edit/"+noteName}, noteName),
 					m("div", [
-						m("a.sn-delete-link", {onclick: function() {
+						m("a.cn-delete-link", {onclick: function() {
 							if (confirm("Are you sure you want to delete this note?")) {
 								localStorage.removeItem(key);
 							}
 						}}, "Delete"),
-						m("a.sn-save-link", {onclick: function() {
+						m("a.cn-save-link", {onclick: function() {
 							var blob = new Blob([localStorage.getItem(key)], {type: "text/plain;charset=utf-8"});
 							FileSaver.saveAs(blob, key+".md");
 						}}, "Save"),
@@ -86,7 +89,7 @@ var NotesList = {
 			if (x > y) {return 1};
 			return 0;
 		});
-		return m("ul.sn-notes-list", list);
+		return m("ul.cn-notes-list", list);
 	}
 }
 
@@ -96,14 +99,14 @@ var NewNoteForm = {
 		vnode.state.valid = false;
 	},
 	view: function(vnode) {
-		let inputField = m("input.sn-create-input", {
+		let inputField = m("input.cn-create-input", {
 			oninput: function(e) {
 				vnode.state.name = e.target.value;
 				vnode.state.valid = /^[a-zA-Z0-9-]+$/.test(vnode.state.name);
 			}
 		});
 		let createButton = m("a", {
-			class: vnode.state.valid ? "sn-create-link" : "sn-create-link sn-create-link-disabled",
+			class: vnode.state.valid ? "cn-create-link" : "cn-create-link cn-create-link-disabled",
 			onclick: function() {
 				if (!vnode.state.valid) {
 					return;
